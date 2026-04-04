19ce03c21719ca29e2dce4dc659ee64c3effbb7b
Ihsan Maulana <dev.ihsanmaulana@gmail.com>
2026-04-04 07:36:34 +0700

    day(5): initialize npm project and explore package management

---
+++ days/day-005/index.js
@@ -0,0 +1,8 @@
+// Mengimport chalk yang sudah kita install dari node_modules
+const chalk = require('chalk');
+
+// Menampilkan teks berwarna di terminal
+console.log(chalk.green('Hello, npm world!'));
+console.log(chalk.blue('This is blue.'));
+console.log(chalk.red('This is red.'));
+console.log(chalk.yellow('This is yellow.'));
